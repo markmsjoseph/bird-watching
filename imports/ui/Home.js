f0d85b2a0db8bf5c071ec68e8986dea051c809fe
Mark Joseph <markmsjoseph@gmail.com>
2018-06-07 01:26:15 -0400

    added bootstrap 4 and react router v4 dynamic url authentication added

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -1,60 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PrivateHeader from './PrivateHeader';
+import { createContainer } from 'meteor/react-meteor-data';
+import {Session} from 'meteor/session';
+
+export class Home extends React.Component {
+
+      constructor(props) {
+        super(props);
+      }
+
+      componentWillMount() {
+          //set the global session variable currentPagePrivacy to the value that was passed in as props from the route component in main.js
+          Session.set('currentPagePrivacy', this.props.priavteOrPublic);//set session id
+      }
+
+      render() {
+          return (
+                  <div>
+                      <div className = "container-fluid header">
+                            <div className = "jumborton">
+                                <p className = "header-loggedInAs text-right">Logged in as:{this.props.username} </p>
+                                  <PrivateHeader  title="Bare Bones App"  />
+
+                            </div>
+                        </div>
+                  </div>
+          );
+      }
+
+}
 
 
-
-export default class Home extends React.Component {
-
-  constructor(props) {
-    super(props);
-        this.state = {
-        username:""
-        };
-  }
-
-    componentWillMount() {
-        if(!Meteor.userId()) {
-          console.log("No user but trying to go back: In ComponentDidMount from Link.js");
-          this.props.history.push('/');
-        }
-
-    }
-
-    componentDidMount() {
-      this.postTracker =  Tracker.autorun(() => {
-          console.log("USERNAME-----------------------:", Meteor.user());
-          if(Meteor.user()){
-            this.setState(()=>{
-              return{
-                username:Meteor.user().username
-              }
-            });
-          }
-          else{
-            console.log("No User");
-          }
-      });
-
-    }
-
-
-    render() {
-      return (
-        <div>
-        <div className = "wrapper wrapper-top">
-                    <PrivateHeader  title="BoilerPlate"  />
-                    <p className = "logged-in-as">Logged in as:{this.state.username} </p>
-
-
-            </div>
-
-
-
-
-        </div>
-      );
-    }
-
-
-  }
+export default createContainer(() => {
+    return {
+      username:Meteor.user() != undefined ? Meteor.user().username : 'undefined'
+    };
+}, Home);
